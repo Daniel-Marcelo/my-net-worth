be330a5412a3bf7b988f8e9f2f13635d3692f6ce
Daniel McKee <danielm.mckee@gmail.com>
2022-07-30 21:04:00 +0100

    Add inputs to calc page

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -1,3 +1,48 @@
+import { FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { x } from "@xstyled/styled-components";
+import { useState } from "react";
+
 export function Calculator() {
-  return <div>Cals</div>;
+  const [, setStartingAmount] = useState(0);
+  const [, setAdditionalContribution] = useState(0);
+  const [contributionPeriod, setContributionPeriod] = useState("");
+
+  return (
+    <x.div p={16}>
+      <Typography variant="h6" component="div" sx={{ textAlign: "center", flexGrow: 1 }}>
+        Quote
+      </Typography>
+
+      <TextField
+        onChange={(event) => setStartingAmount(+event.target.value)}
+        label="Starting Amount"
+        variant="outlined"
+      />
+      <x.div mt={8}>
+        <TextField
+          onChange={(event) => setAdditionalContribution(+event.target.value)}
+          label="Additional Contribution"
+          variant="outlined"
+        />
+
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={contributionPeriod}
+            onChange={(event) => setContributionPeriod(event.target.value)}
+            label="Age"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </x.div>
+    </x.div>
+  );
 }

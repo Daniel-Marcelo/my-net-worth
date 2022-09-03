70d5d6a7b5dfed8004e8276d872307b4ff57d355
Daniel McKee <daniel.mckee@alphasights.com>
2022-09-04 00:08:16 +0100

    Create portfolio context

---
+++ src/context/PortfolioContext.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const PortfolioContext = React.createContext(null);
+
+export const usePortfolioContext = React.useContext(PortfolioContext);
+
+export function PortfolioContextProvider({ children }) {
+  const [portfolios] = useState([]);
+  return <PortfolioContext.Provider value={{ portfolios }}>{children}</PortfolioContext.Provider>;
+}

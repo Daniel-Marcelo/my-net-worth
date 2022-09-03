d6b1209b3d71f38b154bebb7a7460b99979f96ce
Daniel McKee <daniel.mckee@alphasights.com>
2022-09-03 22:08:45 +0100

    Route to login page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@mui/material";
 import { x } from "@xstyled/styled-components";
+import { useNavigate } from "react-router-dom";
 
 export function HomePage() {
+    const navigate = useNavigate();
+
+    const onClickLogin = () => {
+        navigate('/login', { replace: true })
+    }
   return (
     <x.div h="100vh" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" pt={32}>
       <x.div letterSpacing="10px" fontSize="58px">
@@ -9,7 +15,7 @@ export function HomePage() {
       </x.div>
 
       <x.div mt={32}>
-        <Button size="large" variant="contained">
+        <Button size="large" variant="contained" onClick={onClickLogin}>
           <x.span px={48}>LOGIN</x.span>
         </Button>
       </x.div>

0ba4b79bb7843a4141326362ffc4fd41af0c4886
Daniel McKee <daniel.mckee@alphasights.com>
2022-09-03 23:24:19 +0100

    Add password verification

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,19 +1,49 @@
 import { Button, TextField } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { x } from "@xstyled/styled-components";
 import { GoogleLoginButton } from "react-social-login-buttons";
+import passwordValidator from "password-validator";
+import CloseIcon from "@mui/icons-material/Close";
+import CheckIcon from "@mui/icons-material/Check";
 import { useLogin } from "./useLogin";
 
+const schema = new passwordValidator();
+schema
+  .is()
+  .min(8) // Minimum length 8
+  .has()
+  .uppercase() // Must have uppercase letters
+  .has()
+  .lowercase() // Must have lowercase letters
+  .has()
+  .digits(1) // Must have at least 2 digits
+  .is()
+  .not()
+  .oneOf(["Passw0rd", "Password123"]);
+
 export function LoginPage() {
+  const [passwordRulesValid, setPasswordRulesValid] = useState(schema.validate("", { list: true }) as any[]);
   const [loginWithGoogle, loginWithEmailPassword] = useLogin();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  console.log(schema.validate("", { list: true }) as any[]);
+  useEffect(() => {
+    const passwordValid = schema.validate(password, { list: true }) as any[];
+    setPasswordRulesValid(passwordValid);
+  }, [password]);
+
   const onClickLogin = () => {
-    if (email && password) {
+    const passwordValid = schema.validate(password, { list: true });
+    console.log(passwordValid);
+    if (email && passwordValid) {
       loginWithEmailPassword(email, password);
     }
   };
+
+  const renderIcon = (rule: string) =>
+    passwordRulesValid.includes(rule) ? <CloseIcon color="error" /> : <CheckIcon color="success" />;
+
   return (
     <x.div h="100vh" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" pt={32}>
       <x.div letterSpacing="5px" fontSize="32px">
@@ -39,6 +69,28 @@ export function LoginPage() {
               variant="outlined"
             />
           </x.div>
+        </x.div>
+
+        <x.div display="flex" flexDirection="column">
+          <x.span mt={4} mb={0.5}>
+            {renderIcon("min")}
+            <x.span ml={4}>Minimum of 8 characters in length</x.span>
+          </x.span>
+
+          <x.span my={0.5}>
+            {renderIcon("uppercase")}
+            <x.span ml={4}>At least 1 uppercase letter</x.span>
+          </x.span>
+
+          <x.span my={0.5}>
+            {renderIcon("lowercase")}
+            <x.span ml={4}>At least 1 lowercase letter</x.span>
+          </x.span>
+
+          <x.span my={0.5}>
+            {renderIcon("digits")}
+            <x.span ml={4}>At least 1 number</x.span>
+          </x.span>
         </x.div>
 
         <x.div mt={4}>

087a86d5b6b1f6a6de8d6fbc41cfd3af64c136a2
Daniel McKee <daniel.mckee@alphasights.com>
2022-09-03 22:19:50 +0100

    Lint

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,11 @@ import { x } from "@xstyled/styled-components";
 import { useNavigate } from "react-router-dom";
 
 export function HomePage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onClickLogin = () => {
-        navigate('/login', { replace: true })
-    }
+  const onClickLogin = () => {
+    navigate("/login", { replace: true });
+  };
   return (
     <x.div h="100vh" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" pt={32}>
       <x.div letterSpacing="10px" fontSize="58px">

72479de40548cfee931fcb920138e7a182621ec9
Daniel McKee <daniel.mckee@alphasights.com>
2022-09-03 22:03:42 +0100

    Create home title and login button

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,18 @@
-export const HomePage = () => {
-    return <div></div>
+import { Button } from "@mui/material";
+import { x } from "@xstyled/styled-components";
+
+export function HomePage() {
+  return (
+    <x.div h="100vh" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" pt={32}>
+      <x.div letterSpacing="10px" fontSize="58px">
+        MY NET WORTH
+      </x.div>
+
+      <x.div mt={32}>
+        <Button size="large" variant="contained">
+          <x.span px={48}>LOGIN</x.span>
+        </Button>
+      </x.div>
+    </x.div>
+  );
 }

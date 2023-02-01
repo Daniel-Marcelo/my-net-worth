3a72aee821e9824e662ff62bfdacbb02f9cac295
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-02-01 19:39:39 +0000

    Fix null price bug

--- src/pages/QuotePage/useChartData.ts
+++ src/pages/QuotePage/useChartData.ts
@@ -6,7 +6,7 @@ export const useChartData = () => {
   const [chartData, setChartData] = useState([]);
 
   const updateChartData = (result: YF.Result) => {
-    const prices = result.indicators.quote[0].close.map((price) => Math.round(price * 100) / 100);
+    const prices = result.indicators.quote[0].close.filter(data => !!data).map((price) => Math.round(price * 100) / 100);
     const dates = result.timestamp.map((t) => {
       const date = new Date(1970, 0, 1);
       // console.log(t)

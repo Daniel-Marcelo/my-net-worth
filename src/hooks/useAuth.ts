00044e77a9c758d1c72cd7a033d5a9623684318e
daniel mckee <danielmckee@daniels-mini.home>
2023-03-08 07:01:59 +0000

    Add logging

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -22,18 +22,21 @@ export const useLoginWithGoogle = () =>
       provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
       return signInWithPopup(auth, provider);
     },
+    onError: (error) => console.error(error),
   });
 
 export const useLoginUserWithEmail = () =>
   useMutation({
     mutationKey: ["useLoginUserWithEmail"],
     mutationFn: ({ email, password }: EmailPassword) => signInWithEmailAndPassword(auth, email, password),
+    onError: (error) => console.error(error),
   });
 
 export const useCreateUserWithEmail = () =>
   useMutation({
     mutationKey: ["useCreateUserWithEmail"],
     mutationFn: ({ email, password }: EmailPassword) => createUserWithEmailAndPassword(auth, email, password),
+    onError: (error) => console.error(error),
   });
 
 export const useLogout = () => useMutation(() => signOut(auth));

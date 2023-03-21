d7403bb005e358be20ddcc2b3184e129d91b1299
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-03-21 00:34:22 +0000

    Commonize YF service

--- utils/services/yahoo-finance-service.ts
+++ utils/services/yahoo-finance-service.ts
@@ -4,10 +4,13 @@ import { GetEventsResponse } from "../models/events";
 import { GetModulesResponse } from "../models/finance-modules";
 import { PriceChartInterval, PriceChartTimeRange, PriceHistoryResponse } from "../models/price-history";
 
+const cors = "corsDomain=finance.yahoo.com";
+const tsrc = ".tsrc=finance";
+const baseUrl = (queryVersion = 1, apiVersion = 8) =>
+  `https://query${queryVersion}.finance.yahoo.com/v${apiVersion}/finance`;
+
 const searchForTicker = async (ticker: string) => {
-  const response = await axios.get<TickerSearchResponse>(
-    `https://query2.finance.yahoo.com/v1/finance/search?q=${ticker}`
-  );
+  const response = await axios.get<TickerSearchResponse>(`${baseUrl(2, 1)}/search?q=${ticker}`);
   return response.data;
 };
 
@@ -16,19 +19,19 @@ const getPriceHistory = async (
   range = PriceChartTimeRange.OneDay,
   interval = PriceChartInterval.FifteenMins
 ) => {
-  const url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?range=${range}&includePrePost=false&interval=${interval}&corsDomain=finance.yahoo.com&.tsrc=finance`;
+  const url = `${baseUrl()}/chart/${ticker}?rangez=${range}&includePrePost=false&interval=${interval}&${cors}&${tsrc}`;
   const response = await axios.get<PriceHistoryResponse>(url);
   return response.data;
 };
 
 const getModules = async (stock: string, modules: string[]): Promise<GetModulesResponse> => {
-  const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${stock}?modules=${modules.join(",")}`;
+  const url = `${baseUrl(1, 10)}/quoteSummary/${stock}?modules=${modules.join(",")}`;
   const response = await axios.get<GetModulesResponse>(url);
   return response.data;
 };
 
 const getEvents = async (stock: string): Promise<GetEventsResponse> => {
-  const url = `https://query1.finance.yahoo.com/v8/finance/chart/${stock}?interval=1d&period1=0&period2=1674432000&events=div`;
+  const url = `${baseUrl()}/chart/${stock}?interval=1d&period1=0&period2=1674432000&events=div`;
   const response = await axios.get<GetEventsResponse>(url);
   return response.data;
 };

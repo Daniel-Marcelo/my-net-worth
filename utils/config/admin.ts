c39bf92ede4813bbde5d83f97a7c96249976a3bc
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-05-25 18:43:01 -0300

    Remove .replace call

--- utils/config/admin.ts
+++ utils/config/admin.ts
@@ -18,7 +18,7 @@ dotenv.config();
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECT_ID,
-    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    privateKey: process.env.PRIVATE_KEY,
     clientEmail: process.env.CLIENT_EMAIL,
   }),
 });

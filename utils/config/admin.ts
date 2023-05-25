bc1052fd0ff909f528b7ec6bf78f6c425c476b3e
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-05-25 19:02:03 -0300

    Revert last change

--- utils/config/admin.ts
+++ utils/config/admin.ts
@@ -17,7 +17,7 @@ dotenv.config();
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.PROJECT_ID?.replace(/\\n/g, "\n"),
+    projectId: process.env.PROJECT_ID?.replace(/\\n/g, ""),
     privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
     clientEmail: process.env.CLIENT_EMAIL?.replace(/\\n/g, "\n"),
   }),

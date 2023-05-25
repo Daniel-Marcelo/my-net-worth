2a43a2134956f95323608792fa71c91050959a00
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-05-25 19:09:41 -0300

    Remove commented out code

--- utils/config/admin.ts
+++ utils/config/admin.ts
@@ -1,18 +1,6 @@
 import * as admin from "firebase-admin";
 import dotenv from "dotenv";
 
-// require("dotenv").config();
-
-// admin.initializeApp({
-//   credential: admin.credential.cert({
-//     projectId: process.env.PROJECT_ID,
-//     privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
-//     clientEmail: process.env.CLIENT_EMAIL,
-//   }),
-//   databaseURL: process.env.DATABASE_URL,
-// });
-
-// eslint-disable-next-line import/no-absolute-path
 dotenv.config();
 
 admin.initializeApp({

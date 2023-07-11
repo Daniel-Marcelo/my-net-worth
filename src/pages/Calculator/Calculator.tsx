dc3f85b86e5ed721df6866e3787be5fba75ae966
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-07-11 21:16:50 +0100

    Make calcluator responsive

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -1,4 +1,14 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { x } from "@xstyled/styled-components";
 import { useState } from "react";
 import {
@@ -75,7 +85,7 @@ export function Calculator() {
   const [rateOfReturn, setRateOfReturn] = useState(8);
   const [contributionPeriod, setContributionPeriod] = useState(ContributionPeriodValue.Monthly);
   const [years, setYears] = useState(10);
-  const [totalValue, setTotalValue] = useState(0);
+  const [, setTotalValue] = useState(0);
   const [chartData, setChartData] = useState([]);
   const [pieData, setPieData] = useState([]);
   const [clickedCalculate, setClickedCalculate] = useState(false);
@@ -121,93 +131,77 @@ export function Calculator() {
   const getHelperText = (v: number | ContributionPeriodValue) => (v ? "" : "Please enter a value");
 
   return (
-    <x.div display="flex" flexDirection="column" p={16}>
-      <Typography variant="h3" component="div" sx={{ textAlign: "center" }}>
+    <x.div display="flex" flexDirection="column" px={4} gap={4} maxWidth="100%" flex={1}>
+      <Typography variant="h6" component="div" sx={{ textAlign: "center", marginTop: 4 }}>
         Investment Calculator
       </Typography>
-      <x.div display="flex">
-        <x.div display="flex">
-          <x.div mt={2}>
-            <x.div>
-              <TextField
-                value={startingAmount}
-                type="number"
-                onChange={(event) => setStartingAmount(+event.target.value)}
-                label="Starting Amount"
-                variant="outlined"
-                helperText={clickedCalculate && getHelperText(startingAmount)}
-                error={clickedCalculate && isError(startingAmount)}
-              />
-            </x.div>
-            <x.div mt={2}>
-              <TextField
-                value={additionalContribution}
-                type="number"
-                onChange={(event) => setAdditionalContribution(+event.target.value)}
-                label="Additional Contribution"
-                variant="outlined"
-                helperText={clickedCalculate && getHelperText(additionalContribution)}
-                error={clickedCalculate && isError(additionalContribution)}
-              />
-            </x.div>
-            <x.div mt={2}>
-              <FormControl variant="outlined" sx={{ minWidth: 195 }}>
-                <InputLabel id="demo-simple-select-standard-label">Period</InputLabel>
-                <Select
-                  labelId="demo-simple-select-standard-label"
-                  id="demo-simple-select-standard"
-                  label="Period"
-                  value={contributionPeriod}
-                  onChange={(event) => setContributionPeriod(event.target.value as ContributionPeriodValue)}
-                >
-                  {contributionPeriodOptions.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}{" "}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </x.div>
-            <x.div mt={2}>
-              <TextField
-                type="number"
-                value={rateOfReturn}
-                onChange={(event) => setRateOfReturn(+event.target.value)}
-                label="Rate of Return"
-                variant="outlined"
-                helperText={clickedCalculate && getHelperText(rateOfReturn)}
-                error={clickedCalculate && isError(rateOfReturn)}
-              />
-            </x.div>
-            <x.div mt={2}>
-              <TextField
-                value={years}
-                type="number"
-                inputProps={{
-                  min: "1",
-                  step: "1",
-                }}
-                onChange={(event) => {
-                  setYears(+event.target.value);
-                }}
-                label="Years to Grow"
-                variant="outlined"
-                helperText={clickedCalculate && getHelperText(years)}
-                error={clickedCalculate && isError(years)}
-              />
-            </x.div>
-            <Button variant="contained" onClick={validate} sx={{ marginTop: 1 }}>
-              Calculate
-            </Button>
-          </x.div>
-        </x.div>
-        <x.div flex={1} display="flex" h="50vh" mt={4}>
-          {chartData.length ? (
+      <TextField
+        value={startingAmount}
+        type="number"
+        onChange={(event) => setStartingAmount(+event.target.value)}
+        label="Starting Amount"
+        variant="outlined"
+        helperText={clickedCalculate && getHelperText(startingAmount)}
+        error={clickedCalculate && isError(startingAmount)}
+      />
+      <TextField
+        value={additionalContribution}
+        type="number"
+        onChange={(event) => setAdditionalContribution(+event.target.value)}
+        label="Additional Contribution"
+        variant="outlined"
+        helperText={clickedCalculate && getHelperText(additionalContribution)}
+        error={clickedCalculate && isError(additionalContribution)}
+      />
+      <FormControl variant="outlined">
+        <InputLabel id="demo-simple-select-standard-label">Period</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          label="Period"
+          value={contributionPeriod}
+          onChange={(event) => setContributionPeriod(event.target.value as ContributionPeriodValue)}
+        >
+          {contributionPeriodOptions.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <TextField
+        type="number"
+        value={rateOfReturn}
+        onChange={(event) => setRateOfReturn(+event.target.value)}
+        label="Rate of Return"
+        variant="outlined"
+        helperText={clickedCalculate && getHelperText(rateOfReturn)}
+        error={clickedCalculate && isError(rateOfReturn)}
+      />
+      <TextField
+        value={years}
+        type="number"
+        inputProps={{
+          min: "1",
+          step: "1",
+        }}
+        onChange={(event) => setYears(+event.target.value)}
+        label="Years to Grow"
+        variant="outlined"
+        helperText={clickedCalculate && getHelperText(years)}
+        error={clickedCalculate && isError(years)}
+      />
+      <Button variant="contained" onClick={validate}>
+        Calculate
+      </Button>
+      {chartData.length ? (
+        <>
+          <Card sx={{ height: "400px" }} variant="outlined">
             <ResponsiveContainer width="100%" height="100%">
-              <BarChart data={chartData} margin={{ left: 16 }}>
+              <BarChart data={chartData} margin={{ right: 16, top: 12 }}>
                 <CartesianGrid strokeDasharray="1 1 1" />
                 <XAxis dataKey="name" />
-                <YAxis width={60} />
+                <YAxis width={60} fontSize=".75rem" />
                 <Tooltip content={CustomTooltip} />
                 <Legend formatter={(value) => labelize(value)} />
                 <Bar dataKey="startingAmount" stackId="a" fill={calculatorColours.startingAmount} />
@@ -215,56 +209,33 @@ export function Calculator() {
                 <Bar dataKey="interest" stackId="a" fill={calculatorColours.interest} />
               </BarChart>
             </ResponsiveContainer>
-          ) : null}
-        </x.div>
-      </x.div>
-      {/* {totalValue ? (
-        <Typography variant="h6" component="div" sx={{ textAlign: "center", my: 4, flexGrow: 1 }}>
-          {currency}
-          {format(totalValue)}
-        </Typography>
-      ) : null} */}
-      <x.div display="flex" h="50vh" w="100%" mt={4}>
-        {chartData.length ? (
-          // <ResponsiveContainer width="100%" height="100%">
-          //   <BarChart data={chartData} margin={{ left: 0 }}>
-          //     <CartesianGrid strokeDasharray="1 1 1" />
-          //     <XAxis dataKey="name" />
-          //     <YAxis width={60} />
-          //     <Tooltip content={CustomTooltip} />
-          //     <Legend formatter={(value) => labelize(value)} />
-          //     <Bar dataKey="startingAmount" stackId="a" fill={calculatorColours.startingAmount} />
-          //     <Bar dataKey="contributions" stackId="a" fill={calculatorColours.contributions} />
-          //     <Bar dataKey="interest" stackId="a" fill={calculatorColours.interest} />
-          //   </BarChart>
-          // </ResponsiveContainer>
-          <></>
-        ) : null}
-        <x.div h="30vh" w="100%" mt={4}>
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart>
-              <Pie data={pieData} cx="50%" cy="50%" outerRadius={80} fill="#8884d8" dataKey="value">
-                {pieData.map((entry) => (
-                  <Cell key={entry.key} fill={calculatorColours[entry.key]} />
-                ))}
-              </Pie>
-              <Tooltip
-                // eslint-disable-next-line react/no-unstable-nested-components
-                formatter={(value: number, name: string, props) => (
-                  // console.log(value)
-                  // console.log(name)
-                  // console.log(props)
-                  // eslint-disable-next-line react/prop-types
-                  <x.span color={calculatorColours[props.payload.key]}>
-                    {currency}
-                    {format(value)}
-                  </x.span>
-                )}
-              />
-            </PieChart>
-          </ResponsiveContainer>
-        </x.div>
-      </x.div>
+          </Card>
+          <Card sx={{ height: "400px" }} variant="outlined">
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie data={pieData} cx="50%" cy="50%" outerRadius={120} fill="#8884d8" dataKey="value">
+                  {pieData.map((entry) => (
+                    <Cell key={entry.key} fill={calculatorColours[entry.key]} />
+                  ))}
+                </Pie>
+                <Tooltip
+                  // eslint-disable-next-line react/no-unstable-nested-components
+                  formatter={(value: number, name: string, props) => (
+                    // console.log(value)
+                    // console.log(name)
+                    // console.log(props)
+                    // eslint-disable-next-line react/prop-types
+                    <x.span color={calculatorColours[props.payload.key]}>
+                      {currency}
+                      {format(value)}
+                    </x.span>
+                  )}
+                />
+              </PieChart>
+            </ResponsiveContainer>
+          </Card>
+        </>
+      ) : null}
     </x.div>
   );
 }

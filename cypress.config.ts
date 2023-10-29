221a2f6c864a7d6c9f2423fdf9d4efaba032f96d
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-10-29 17:01:22 +0000

    Configure additional cypress log command

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 import fs from "fs";
 
+const fileName = "./similar-ftse-100.json";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
@@ -9,6 +10,23 @@ export default defineConfig({
           const data = JSON.parse(fs.readFileSync("./similar.json"));
           fs.writeFileSync(
             `./similar.json`,
+            JSON.stringify({
+              ...data,
+              ...json,
+            })
+          );
+          console.log({
+            ...data,
+            ...json,
+          });
+
+          return null;
+        },
+        logFtse100(json) {
+          console.log("logging to ftse file");
+          const data = JSON.parse(fs.readFileSync(fileName));
+          fs.writeFileSync(
+            fileName,
             JSON.stringify({
               ...data,
               ...json,

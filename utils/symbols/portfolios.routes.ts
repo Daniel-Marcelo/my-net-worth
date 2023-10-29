3ce614a503c49aed295b3ed39abbab0ef2e031ea
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-10-29 17:37:45 +0000

    comment out service

--- utils/symbols/portfolios.routes.ts
+++ utils/symbols/portfolios.routes.ts
@@ -1,29 +1,27 @@
 import * as express from "express";
-import { symbolService } from "./symbol-service";
-import { DeletePortfolioEntriesRequest, PortfolioEntry } from "../models/PortfolioEntry";
 
 const symbolsRouter = express.Router();
 
 symbolsRouter.get("/:id", async (req, res) => {
-  const portfolioId = req.params.id;
-  const data = await portfolioService.get(portfolioId);
-  res.send(JSON.stringify(data));
+  // const portfolioId = req.params.id;
+  // const data = await portfolioService.get(portfolioId);
+  res.send(JSON.stringify({ success: true }));
 });
 
 symbolsRouter.get("", async (req, res) => {
-  const data = await portfolioService.getList();
-  res.send(JSON.stringify(data));
+  // const data = await portfolioService.getList();
+  res.send(JSON.stringify({ success: true }));
 });
 
 symbolsRouter.post("", async (req, res) => {
-  const portfolioEntry = req.body as PortfolioEntry;
-  await portfolioService.create(portfolioEntry);
+  // const portfolioEntry = req.body as PortfolioEntry;
+  // await portfolioService.create(portfolioEntry);
   res.send(JSON.stringify({ success: true }));
 });
 
 symbolsRouter.delete("", async (req, res) => {
-  const portfolioEntry = req.body as DeletePortfolioEntriesRequest;
-  await portfolioService.deleteList(portfolioEntry.ids);
+  // const portfolioEntry = req.body as DeletePortfolioEntriesRequest;
+  // await portfolioService.deleteList(portfolioEntry.ids);
   res.send(JSON.stringify({ success: true }));
 });
 

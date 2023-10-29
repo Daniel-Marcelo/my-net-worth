9fadc0c07a1b7b1b2ce95d3124caa2e55707b930
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2023-10-29 17:21:48 +0000

    Duplicate cypress test

--- cypress/e2e/get-similar-tickers.cy.ts
+++ cypress/e2e/get-similar-tickers.cy.ts
@@ -1,36 +1,41 @@
 import { tickers } from "./tickers";
+import { tickersFtse100 } from "./tickers-ftse-100";
 
 describe("Get similar tickers", () => {
-  tickers.forEach((ticker) => {
-    it("should fetch", () => {
-      // cy.on("uncaught:exception", (err, runnable) => {
-      cy.on("uncaught:exception", (err, runnable) => {
-        return false;
-      });
-      cy.visit("finance.yahoo.com/quote/" + ticker);
-
-      Cypress.on("fail", (error, runnable) => {
-        // if () {
-        console.log(error);
-        // throw error;
-        // }
-      });
-      cy.get(`button:contains(Accept all)`).click();
-      cy.get(`button:contains(Maybe later)`).click();
-
-      // });
-      cy.scrollTo(0, 500);
-      const text = `Similar to ${ticker}`;
-      cy.get(`span:contains(${text})`);
-      const similar = [];
-      cy.get("#similar-by-symbol > table > tbody > tr > td > a")
-        .each((item) => {
-          similar.push(item[0].innerText);
-        })
-        .then(() => {
-          const json = { [ticker]: similar };
-          cy.task("log", json);
+  ["ULVR.L"].forEach((ticker) => {
+    describe(`${ticker}`, () => {
+      it("navigates to url ", () => {
+        cy.on("uncaught:exception", (err, runnable) => {
+          return false;
         });
+        cy.visit("finance.yahoo.com/quote/" + ticker);
+
+        // Cypress.on("fail", (error, runnable) => {
+        //   console.log(error);
+        // });
+        cy.get(`button:contains(Accept all)`).click();
+        // cy.get(`button:contains(Maybe later)`).click();
+        // });
+        // it("should fetch", () => {
+        cy.scrollTo(0, 500);
+        const text = `Similar to ${ticker}`;
+        console.log("checking for similar to");
+        // cy.get(`span:contains(${text})`);
+        cy.get(`Market Cap`);
+
+        const similar = [];
+        console.log("GETTING");
+        // cy.get("#similar-by-symbol > table > tbody > tr > td > a")
+        cy.get('td[data-test="MARKET_CAP-value"]')
+          .each((item) => {
+            console.log("in each");
+            similar.push(item[0].innerText);
+          })
+          .then(() => {
+            const json = { [ticker]: similar };
+            cy.task("logFtse100", json);
+          });
+      });
     });
   });
 });
